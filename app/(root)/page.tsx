2a81455a616e5e5af7241c50d8896fda2b88ad68
nkansah-wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-12-08 21:42:47 -0600

    feat: Hero UI setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,16 @@
 
 export default function Home() {
   return (
-   <>
-    <article>This is a home page</article>
-   </>  );
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your StartUp, <br/>
+          Connect with entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+            Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+
+      </section>
+    </>);
 }
